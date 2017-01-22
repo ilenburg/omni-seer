8345d9bc17fcc9eec76c91df120008956e6b1abb
Ian Ilenburg <ian.ilenburg@gmail.com>
2017-01-22 19:19:43 -0200

    Project setup and OpenGL ES basic view

---
+++ app/src/main/java/com/lonewolf/lagom/GameRenderer.java
@@ -0,0 +1,36 @@
+package com.lonewolf.lagom;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by Ian on 22/01/2017.
+ */
+
+public class GameRenderer implements GLSurfaceView.Renderer {
+
+    private final Context context;
+
+    public GameRenderer(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.5f, 0.75f, 1f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+}
---
+++ app/src/main/java/com/lonewolf/lagom/GameView.java
@@ -0,0 +1,20 @@
+package com.lonewolf.lagom;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+/**
+ * Created by Ian on 22/01/2017.
+ */
+
+public class GameView extends GLSurfaceView {
+
+    private GameRenderer gameRenderer;
+
+    public GameView(Context context) {
+        super(context);
+        this.gameRenderer = new GameRenderer(context);
+        setRenderer(gameRenderer);
+    }
+
+}
