75313db298f40747444811273d1863654221343f
Ian Ilenburg <ian.ilenburg@gmail.com>
2017-08-30 21:29:01 -0300

    The Night is Dark and Full of Terrors

--- app/src/main/res/raw/tex_transition_scroll_frag.glsl
+++ app/src/main/res/raw/tex_transition_scroll_frag.glsl
@@ -8,18 +8,9 @@ void main() {
 
     vec2 texPos = vec2(texCoord.x + fract(scroll), texCoord.y);
 
-    float variation = abs(sin(time / 100.0f) * 3.0f);
+    float variation = abs(sin(time / 300.0f)) + 0.5f;
 
-    if(variation > 1.0f) {
-        if (variation < 2.0f) {
-            variation = variation - 1.0f;
-        }
-        else {
-            variation = 1.0f;
-        }
-    }
-    else {
-        variation = 0.0f;
-    }
+    variation = min(variation, 1.0f);
+
     gl_FragColor = ((texture2D(tex, texPos)) * (1.0f - variation)) + (texture2D(tex2, texPos) * variation);
 }
