ddaf831307b6d10b7610c4975fc03ad96b731baa
Ian Ilenburg <ian.ilenburg@gmail.com>
2018-03-02 20:33:53 -0300

    AdMob

--- app/src/main/java/com/lonewolf/lagom/activities/MainActivity.java
+++ app/src/main/java/com/lonewolf/lagom/activities/MainActivity.java
@@ -1,11 +1,19 @@
 package com.lonewolf.lagom.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.lonewolf.lagom.graphics.GameView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,9 +23,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.gameView = new GameView(this);
-        setContentView(gameView);
+        gameView = new GameView(this);
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
+
+        RelativeLayout layout = new RelativeLayout(this);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup
+                .LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.SMART_BANNER);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice
+                ("79932D975E5085B56B15E1182AEB63CC").build();
+        adView.loadAd(adRequest);
+        adView.setBackgroundColor(Color.TRANSPARENT);
+
+        adView.setLayoutParams(params);
+
+        layout.addView(gameView);
+        layout.addView(adView);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(layout);
     }
 
     @Override
